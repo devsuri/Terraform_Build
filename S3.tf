25775bc836c286e7437e779919b865dbfcacb461
chitransh512 <39611249+chitransh512@users.noreply.github.com>
2020-01-22 12:28:42 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,10 +1,10 @@
-variable "Access_key" {
-  default = "AWS_ACCESS_KEY_ID"
-}
+#variable "Access_key" {
+ # default = "AWS_ACCESS_KEY_ID"
+#}
 
-variable "Secret_Key" {
-  default = "AWS_SECRET_ACCESS_KEY"
-}
+#variable "Secret_Key" {
+ # default = "AWS_SECRET_ACCESS_KEY"
+#}
 
 
 provider "aws" {
