b10325b3b8c2dc99f2c573005b6598955a0ff447
chitransh512 <39611249+chitransh512@users.noreply.github.com>
2020-01-22 14:45:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
-variable "Access_key" {
-  default = "$AWS_ACCESS_KEY_ID"
-}
 
-variable "Secret_Key" {
-  default = "$AWS_SECRET_ACCESS_KEY"
-}
+  # variable "Access_key" {
+ #  default = "$AWS_ACCESS_KEY_ID"
+# }
+
+# variable "Secret_Key" {
+#   default = "$AWS_SECRET_ACCESS_KEY"
+# }
