83d1d48d213e5cc0035277c4027e185c22c9dd6c
chitransh512 <39611249+chitransh512@users.noreply.github.com>
2020-01-22 11:58:27 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -4,10 +4,6 @@ provider "aws" {
   region  = "us-west-1"
   access_key = "AWS_ACCESS_KEY_ID"
   secret_key = "AWS_SECRET_ACCESS_KEY"
-  },
-
-  assume_role {
-    role_arn     = "arn:aws:iam::361061974256:role/service-role/codebuild-CodePipeline_Chitransh-service-role"
 }
 
 
