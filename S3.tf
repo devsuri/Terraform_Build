c1a0703f2a38ad6b2803811fb95c7b1bdf1cd4a2
chitransh512 <39611249+chitransh512@users.noreply.github.com>
2020-01-22 12:15:49 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -2,8 +2,8 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "us-west-1"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  access_key = "${var.Access_key}"
+  secret_key = "${var.Secret_Key}"
 }
 
 
