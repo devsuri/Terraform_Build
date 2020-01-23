d40a3b6781a9feb94173a90c672ebf10161b0586
chitransh512 <39611249+chitransh512@users.noreply.github.com>
2020-01-23 10:37:05 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -10,13 +10,11 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "us-west-1"
- # access_key = "${var.Access_key}"
- # secret_key = "${var.Secret_Key}"
 }
 
 
 resource "aws_s3_bucket" "b" {
-  bucket = "terraformcodebuildbucketbyterraform"
+  bucket = "terraformcodebuildbucketbyterraform_2"
   acl    = "private"
 
   tags = {
